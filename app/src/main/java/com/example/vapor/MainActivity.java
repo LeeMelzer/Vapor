6a5fbd754ea5875ab9f9e911f05ebeeec4922802
Lee <lee.melzer@gmail.com>
2023-04-15 19:16:35 -0700

    Design complete. No functionality in buttons yet. Will update intents once all classes are complete.

--- app/src/main/java/com/example/vapor/MainActivity.java
+++ app/src/main/java/com/example/vapor/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.vapor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        accountButton();
+    }
+
+    private void accountButton() {
+        Button account = findViewById(R.id.accountButton);
+        account.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent (MainActivity.this, AccountPage.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
