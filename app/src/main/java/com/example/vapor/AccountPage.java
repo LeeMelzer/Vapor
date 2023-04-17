8798102bd0c7531efccc29b8b4071d05bd435968
Dominic Renales <Renales.Dominic@gmail.com>
2023-04-17 15:29:13 -0700

    Finished Barebones Library
    - Library page connected
    - Account library button leads to library page
    - Library page navbar "account" leads to account page

--- app/src/main/java/com/example/vapor/AccountPage.java
+++ app/src/main/java/com/example/vapor/AccountPage.java
@@ -2,7 +2,9 @@ package com.example.vapor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AccountPage extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class AccountPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_page);
+    }
+
+    public void accountToLibrary(View view) {
+        Intent intent = new Intent (AccountPage.this, AccountLibrary.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
