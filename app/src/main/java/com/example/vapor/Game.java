8f84e2e9db8f77f4a511ee2b2bc77bffe0c666c2
rasinnovation <54919525+rasinnovation@users.noreply.github.com>
2023-04-18 15:29:36 -0700

    Added broken java classes and layouts

---
+++ app/src/main/java/com/example/vapor/Game.java
@@ -0,0 +1,38 @@
+package com.example.vapor;
+
+public class Game {
+
+    private int mIconResourceId;
+    private String mTitle;
+    private String mDescription;
+    private double mPrice;
+
+    public Game(int iconResourceId, String title, String description, double price) {
+        mIconResourceId = iconResourceId;
+        mTitle = title;
+        mDescription = description;
+        mPrice = price;
+    }
+
+    public int getIconResourceId() {
+        return mIconResourceId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public double getPrice() {
+        return mPrice;
+    }
+
+    public void setPrice(double price) {
+        mPrice = price;
+    }
+
+    // You can add other methods as needed
+}
