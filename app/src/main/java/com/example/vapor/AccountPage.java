c8ad0ac5b56447ced2f610b6b68ec52b7531e28e
Lee <lee.melzer@gmail.com>
2023-04-18 18:47:01 -0700

    Updated login page with sign in button. Updated account page with nav bar. Will finish button mappings on nav bar once all classes complete.

--- app/src/main/java/com/example/vapor/AccountPage.java
+++ app/src/main/java/com/example/vapor/AccountPage.java
@@ -1,10 +1,15 @@
 package com.example.vapor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class AccountPage extends AppCompatActivity {
 
@@ -12,6 +17,35 @@ public class AccountPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_page);
+
+        BottomNavigationView bnv = findViewById(R.id.bnv_navbar);
+        bnv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                //if(id == R.id.account)
+                switch(id) {
+                    case R.id.home:
+                        Intent library = new Intent(getApplicationContext(), AccountLibrary.class);
+                        library.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(library);
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.store:
+                        System.out.println("Store button pressed");
+                        return true;
+
+                    case R.id.account:
+                        Intent account = new Intent(getApplicationContext(), AccountPage.class);
+                        account.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(account);
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void accountToLibrary(View view) {
