445e07262a60c2a32935f6d55bb32a264faba083
Dominic Renales <Renales.Dominic@gmail.com>
2023-04-23 11:41:17 -0700

    Manual Port
    - Added changes from Lee for testing

--- app/src/main/java/com/example/vapor/AccountPage.java
+++ app/src/main/java/com/example/vapor/AccountPage.java
@@ -5,64 +5,247 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class AccountPage extends AppCompatActivity {
 
+    private Account currentAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_page);
+        currentAccount = new Account();
+        initTextChangedEvents();
+        initSaveButton();
 
         BottomNavigationView bnv = findViewById(R.id.bnv_navbar);
         bnv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
+                //if(id == R.id.account)
+                switch(id) {
+                    case R.id.home:
+                        Intent library = new Intent(AccountPage.this, AccountLibrary.class);
+                        library.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(library);
+                        overridePendingTransition(0,0);
+                        return true;
 
-                // Using the id, we can determine which item was selected
-                if (id == R.id.home) {
-                    // Create a new Intent object
-                    Intent home = new Intent(getApplicationContext(), AccountLibrary.class);
-                    // Set the flags for the Intent object
-                    home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    // Start the activity
-                    startActivity(home);
-                    overridePendingTransition(0,0);
-                    return true;
-                }
-                else if (id == R.id.store) {
-                    // Create a new Intent object
-                    Intent store = new Intent(getApplicationContext(), AccountStoreActivity.class);
-                    // Set the flags for the Intent object
-                    store.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    // Start the activity
-                    startActivity(store);
-                    overridePendingTransition(0,0);
-                    return true;
-                }
-                else if (id == R.id.account) {
-                    // Create a new Intent object
-                    Intent account = new Intent(getApplicationContext(), AccountPage.class);
-                    // Set the flags for the Intent object
-                    account.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    // Start the activity
-                    startActivity(account);
-                    overridePendingTransition(0,0);
-                    return true;
+                    case R.id.store:
+                        Intent store = new Intent(AccountPage.this, AccountStoreActivity.class);
+                        store.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(store);
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.account:
+                        Intent account = new Intent(getApplicationContext(), AccountPage.class);
+                        account.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(account);
+                        overridePendingTransition(0,0);
+                        return true;
                 }
                 return false;
             }
         });
     }
 
-    public void accountToLibrary(View view) {
-        Intent intent = new Intent (AccountPage.this, AccountLibrary.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
+    private void initTextChangedEvents() {
+        final EditText etUsername = findViewById(R.id.username);
+        etUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                currentAccount.setUsername(etUsername.getText().toString());
+            }
+        });
+
+        final EditText etFName = findViewById(R.id.firstName);
+        etFName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                currentAccount.setFName(etFName.getText().toString());
+            }
+        });
+
+        final EditText etLName = findViewById(R.id.lastName);
+        etLName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                currentAccount.setLName(etLName.getText().toString());
+            }
+        });
+
+        final EditText etEmail = findViewById(R.id.eAddress);
+        etEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                currentAccount.setEmail(etEmail.getText().toString());
+            }
+        });
+
+        final EditText etPhone = findViewById(R.id.pNumber);
+        etPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                currentAccount.setPhone(etPhone.getText().toString());
+            }
+        });
+
+        final EditText etAddress = findViewById(R.id.address);
+        etAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                currentAccount.setAddress(etAddress.getText().toString());
+            }
+        });
+
+        final EditText etCardNumber = findViewById(R.id.cardNumber);
+        etCardNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                currentAccount.setCardNumber(etCardNumber.getText().toString());
+            }
+        });
+
+        final EditText etCardCode = findViewById(R.id.securityCode);
+        etCardCode.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                currentAccount.setCardCode(etCardCode.getText().toString());
+            }
+        });
+
+        final EditText etExpirationMonth = findViewById(R.id.expirationMonth);
+        etExpirationMonth.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                currentAccount.setExpirationMonth(etExpirationMonth.getText().toString());
+            }
+        });
+
+        final EditText etExpirationYear = findViewById(R.id.expirationYear);
+        etExpirationYear.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Auto-generated method
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                currentAccount.setExpirationYear(etExpirationYear.getText().toString());
+            }
+        });
+    }
+
+    private void initSaveButton() {
+        Button saveButton = findViewById(R.id.updateButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                boolean wasSuccessful;
+                VaporDataSource ds = new VaporDataSource(AccountPage.this);
+                try {
+                    ds.open();
+                    if (currentAccount.getUid() == -1) {
+                        wasSuccessful = ds.insertUser(currentAccount);
+                        int newUid = ds.getLastUid();
+                        currentAccount.setUid(newUid);
+                    }
+                    else {
+                        wasSuccessful = ds.updateUser(currentAccount);
+                    }
+                    ds.close();
+                }
+                catch (Exception e) {
+                    wasSuccessful = false;
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/vapor/VaporDataSource.java
@@ -0,0 +1,93 @@
+package com.example.vapor;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class VaporDataSource {
+
+    private SQLiteDatabase database;
+    private vaporDBHelper dbHelper;
+
+    public VaporDataSource(Context context) {
+        dbHelper = new vaporDBHelper(context);
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public boolean insertUser(Account a) {
+        boolean didSucceed = false;
+        try {
+            ContentValues initialValues = new ContentValues();
+
+            initialValues.put("username", a.getUsername());
+            initialValues.put("fName", a.getFName());
+            initialValues.put("lName", a.getLName());
+            initialValues.put("email", a.getEmail());
+            initialValues.put("phone", a.getPhone());
+            initialValues.put("address", a.getAddress());
+            initialValues.put("card_number", a.getCardNumber());
+            initialValues.put("card_code", a.getCardCode());
+            initialValues.put("expiration_month", a.getExpirationMonth());
+            initialValues.put("expiration_year", a.getExpirationYear());
+
+            didSucceed = database.insert("user_account", null, initialValues) > 0;
+        }
+        catch (Exception e) {
+            // will return false if there is an exception
+        }
+        return didSucceed;
+    }
+
+    public boolean updateUser(Account a) {
+        boolean didSucceed = false;
+        try {
+            Long rowId = (long) a.getUid();
+            ContentValues updateValues = new ContentValues();
+
+            updateValues.put("username", a.getUsername());
+            updateValues.put("fName", a.getFName());
+            updateValues.put("lName", a.getLName());
+            updateValues.put("email", a.getEmail());
+            updateValues.put("phone", a.getPhone());
+            updateValues.put("address", a.getAddress());
+            updateValues.put("card_number", a.getCardNumber());
+            updateValues.put("card_code", a.getCardCode());
+            updateValues.put("expiration_month", a.getExpirationMonth());
+            updateValues.put("expiration_year", a.getExpirationYear());
+
+            didSucceed = database.update("user_account", updateValues, "uid=" + rowId, null) > 0;
+            if (didSucceed) {
+                System.out.println("Successfully updated");
+            }
+        }
+        catch (Exception e) {
+            // will return false if there is an exception
+        }
+        return didSucceed;
+    }
+
+    public int getLastUid() {
+        int lastUid;
+        try {
+            String query = "select MAX(uid) from user_account";
+            Cursor cursor = database.rawQuery(query, null);
+
+            cursor.moveToFirst();
+            lastUid = cursor.getInt(0);
+            cursor.close();
+        }
+        catch (Exception e) {
+            lastUid = -1;
+        }
+        return lastUid;
+    }
+}
