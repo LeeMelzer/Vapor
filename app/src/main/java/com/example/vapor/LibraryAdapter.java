76f4686feeeb2a8d57f3e9422fda4b83a2c8303f
Dominic Renales <Renales.Dominic@gmail.com>
2023-04-28 21:35:20 -0700

    JOB'S DONE

--- app/src/main/java/com/example/vapor/LibraryAdapter.java
+++ app/src/main/java/com/example/vapor/LibraryAdapter.java
@@ -1,7 +1,10 @@
 package com.example.vapor;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.io.InputStream;
 import java.util.List;
 
 public class LibraryAdapter extends ArrayAdapter<LibraryGame> {
@@ -30,9 +34,30 @@ public class LibraryAdapter extends ArrayAdapter<LibraryGame> {
 
         ImageView image = convertView.findViewById(R.id.gameImageLibrary);
         TextView title = convertView.findViewById(R.id.gameTitleLibrary);
-        image.setImageURI(Uri.parse(game.getImageURL()));
+
+        new DownloadImageFromInternet((ImageView) image).execute(game.getImageURL());
         title.setText(game.getTitle());
 
         return convertView;
+    }
+
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView=imageView;
+        }
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL=urls[0];
+            Bitmap bimage=null;
+            try {
+                InputStream in=new java.net.URL(imageURL).openStream();
+                bimage= BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+            }
+            return bimage;
+        }
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
     }
 }
