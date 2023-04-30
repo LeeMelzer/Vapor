60d8e0d16b2f437a180c716b0f7c7f680c9b01f7
Scott Hawley <iamultima0@gmail.com>
2023-04-29 19:36:50 -0700

    updated navbar to pass uid

--- app/src/main/java/com/example/vapor/GamePage.java
+++ app/src/main/java/com/example/vapor/GamePage.java
@@ -57,42 +57,45 @@ public class GamePage extends AppCompatActivity {
         });
 
         BottomNavigationView bnv = findViewById(R.id.bnv_navbar);
-        bnv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
+        bnv.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            //if(id == R.id.account)
 
-                // Using the id, we can determine which item was selected
-                if (id == R.id.home) {
-                    // Create a new Intent object
-                    Intent home = new Intent(getApplicationContext(), AccountLibrary.class);
-                    // Set the flags for the Intent object
-                    home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    // Start the activity
-                    startActivity(home);
-                    overridePendingTransition(0, 0);
-                    return true;
-                } else if (id == R.id.store) {
-                    // Create a new Intent object
-                    Intent store = new Intent(getApplicationContext(), AccountStoreActivity.class);
-                    // Set the flags for the Intent object
-                    store.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    // Start the activity
-                    startActivity(store);
-                    overridePendingTransition(0, 0);
-                    return true;
-                } else if (id == R.id.account) {
-                    // Create a new Intent object
-                    Intent account = new Intent(getApplicationContext(), AccountPage.class);
-                    // Set the flags for the Intent object
-                    account.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    // Start the activity
-                    startActivity(account);
-                    overridePendingTransition(0, 0);
-                    return true;
-                }
-                return false;
+            // Using the id, we can determine which item was selected
+            if (id == R.id.home) {
+                // Create a new Intent object
+                Intent home = new Intent(getApplicationContext(), AccountLibrary.class);
+                // Set the flags for the Intent object
+                home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                home.putExtra("uid", 1);
+                // Start the activity
+                startActivity(home);
+                overridePendingTransition(0,0);
+                return true;
             }
+            else if (id == R.id.store) {
+                // Create a new Intent object
+                Intent store = new Intent(getApplicationContext(), AccountStoreActivity.class);
+                // Set the flags for the Intent object
+                store.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                store.putExtra("uid", 1);
+                // Start the activity
+                startActivity(store);
+                overridePendingTransition(0,0);
+                return true;
+            }
+            else if (id == R.id.account) {
+                // Create a new Intent object
+                Intent account = new Intent(getApplicationContext(), AccountPage.class);
+                // Set the flags for the Intent object
+                account.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                account.putExtra("uid", 1);
+                // Start the activity
+                startActivity(account);
+                overridePendingTransition(0,0);
+                return true;
+            }
+            return false;
         });
     }
 
